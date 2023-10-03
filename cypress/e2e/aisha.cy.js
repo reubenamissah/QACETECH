cda7c622b0b41ad5990dbe43f5841a184600fa05
Adedeji Omotolani <lafiaji@Lafiajis-MacBook-Air.local>
2023-10-03 16:02:00 +0100

    Aisha's valid login test to Konga

---
+++ cypress/e2e/aisha.cy.js
@@ -0,0 +1,24 @@
+import {aisha} from "../fixtures/selector.js";
+
+Cypress.on('uncaught:exception', (err, runnable) =>{
+  // returning false here prevents Cypress from
+  // failing test
+  return false
+
+})
+describe("Given I am on the Konga page", function () {
+    beforeEach(function () {
+        cy.visit('/')
+    });
+    
+    it("AISHA - I Should be able to sign in my account with valid credentials", function () {
+      cy.get(aisha.loginBttn).click()
+      cy.get(aisha.emailFieeld).click()
+      cy.get(aisha.emailFieeld).type(aisha.emailText)
+      cy.get(aisha.passwrdField).click()
+      cy.get(aisha.passwrdField).type(aisha.passwordText)
+      cy.get(aisha.loginButtn).click()
+      cy.wait(5000)
+
+    })
+})
--- cypress/fixtures/selector.js
+++ cypress/fixtures/selector.js
@@ -6,3 +6,11 @@ export const invalidlogin ={
     passwordTxt:"testing1234@",
     loginButton:".eed68_3IZCC > ._2aac2_3bwnD > ._0a08a_3czMG"
 }
+export const aisha ={
+    loginBttn:"._12e27_1r3kc > ._7ad32_SD12Y",
+    emailFieeld:"#username",
+    emailText:'kit@yopmail.com',
+    passwrdField:"#password",
+    passwordText:"Password@10",
+    loginButtn:".eed68_3IZCC > ._2aac2_3bwnD > ._0a08a_3czMG"
+}
